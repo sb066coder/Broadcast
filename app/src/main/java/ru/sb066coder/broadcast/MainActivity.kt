c2366bb2a46087e5b8ad68447ce830ee3de12b60
sergey <sergey066@yandex.ru>
2023-07-11 21:42:59 +0300

    Custom receiver added

--- app/src/main/java/ru/sb066coder/broadcast/MainActivity.kt
+++ app/src/main/java/ru/sb066coder/broadcast/MainActivity.kt
@@ -4,18 +4,29 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
 
+
+    private var countClicks = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            Intent().apply {
+                action = MyReceiver.ACTION_CLICKED
+                putExtra(MyReceiver.CLICKS, ++countClicks)
+                sendBroadcast(this)
+            }
+        }
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(MyReceiver.ACTION_CLICKED)
         }
         registerReceiver(receiver, intentFilter)
     }
--- app/src/main/java/ru/sb066coder/broadcast/MyReceiver.kt
+++ app/src/main/java/ru/sb066coder/broadcast/MyReceiver.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action) {
+            ACTION_CLICKED -> {
+                val clicks = intent.getIntExtra(CLICKS, -1)
+                Toast.makeText(context, "Button clicked $clicks times", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(
@@ -20,5 +24,10 @@ class MyReceiver : BroadcastReceiver() {
                 Toast.makeText(context, "Low battery!!!", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
+        const val CLICKS = "clicks"
     }
 }
